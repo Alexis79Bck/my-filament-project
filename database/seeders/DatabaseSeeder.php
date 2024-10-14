6aeb5fbe449d7ef750352118a8345b1354fd2c39
Alexis Eduardo Mata <alexedmata@gmail.com>
2024-10-14 16:31:45 -0400

    Instalado Larave World con losSeeders de Paises y Estados

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -15,7 +15,8 @@ class DatabaseSeeder extends Seeder
     public function run(): void
     {
         // User::factory(10)->create();
-
+        $this->call(WorldTableSeeder::class);
+        
         User::create([
             'name' => 'SuperAdmin',
             'email' => 'superadmin@testing.com',
